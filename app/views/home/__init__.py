a5543d86c656d1e6f7ffd4a61aa2fa0468f0ab2d
twd10 <tzuoyeah@gmail.com>
2026-01-05 17:02:58 +0800

    password

--- app/views/home/__init__.py
+++ app/views/home/__init__.py
@@ -27,13 +27,15 @@ def choose_user():
         flash('登入成功！', 'success') 
         return render_template('home/chooseUser.html')
     else:
-        error_html = """
-        <div class="flex items-center gap-2 text-error text-sm font-medium animate-pulse">
-            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-4 w-4" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            <span>密碼錯誤，請重試！</span>
-        </div>
-        """
-        return error_html
+        resp = make_response()
+        trigger_data = {
+            "login-error": {
+                "message": "密碼錯誤，請重新輸入！",
+                "type": "error"
+            }
+        }
+        resp.headers['HX-Trigger'] = json.dumps(trigger_data)
+        return resp
 
 @blueprint.route('/choose_region/', methods=['POST','GET'])
 def choose_region():
